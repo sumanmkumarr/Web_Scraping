b79c8505d398abe517dfe3e79ca3f048484bc532
suman kumar <sumang91222@gmail.com>
2025-06-24 22:59:41 +0530

    extract_data_from_table

--- extract_table_data.py
+++ extract_table_data.py
@@ -14,16 +14,33 @@ table = soup.find("table",class_ ="table table-sm table-hover screenertable")
 header = table.find_all("th")
 
 
+# insert all the data into the list
+table_data  = table.find_all("tr")
+table_actual = []
+for i in table_data:
+    table_actual.append(i.text)
+
+# print(table_actual)
+
+df = pd.DataFrame(table_actual)
+print(df)
+df.to_csv("stock.csv")
+
+
 # insert all the company name into the list 
-company_name = table.find_all("a",class_="complink")
-company = []
-for i in company_name:
-    company.append(i.text)
+# company_name = table.find_all("a",class_="complink")
+# company = []
+# for i in company_name:
+#     company.append(i.text)
+
+
 
 
 # crteate a dataframe using pandas
-df = pd.DataFrame({"company":company})
-print(df)
+# df = pd.DataFrame({"company":company})
+# print(df)
+
+
 
 # save into  csv file
-df.to_csv("company_detail.csv")
+# df.to_csv("company_detail.csv")
